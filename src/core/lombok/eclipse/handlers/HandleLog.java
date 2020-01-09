35038a029818e178eed42f7cb81f5eca3dd6563c
junjie.fu <junjie.fu@united-imaging.com>
2020-01-09 14:32:51 +0800

    添加eclipse handler

--- src/core/lombok/eclipse/handlers/HandleLog.java
+++ src/core/lombok/eclipse/handlers/HandleLog.java
@@ -266,7 +266,18 @@ public class HandleLog {
 			processAnnotation(LoggingFramework.SLF4J, annotation, source, annotationNode, annotation.getInstance().topic());
 		}
 	}
-	
+
+	/**
+	 * Handles the {@link lombok.extern.uih.log.UIHLog} annotation for Eclipse.
+	 */
+	@ProviderFor(EclipseAnnotationHandler.class)
+	public static class HandleUIHLog extends EclipseAnnotationHandler<lombok.extern.uih.log.UIHLog> {
+		@Override public void handle(AnnotationValues<lombok.extern.uih.log.UIHLog> annotation, Annotation source, EclipseNode annotationNode) {
+			handleFlagUsage(annotationNode, ConfigurationKeys.LOG_SLF4J_FLAG_USAGE, "@UIHLog", ConfigurationKeys.LOG_ANY_FLAG_USAGE, "any @Log");
+			processAnnotation(LoggingFramework.UIHLOG, annotation, source, annotationNode, annotation.getInstance().topic());
+		}
+	}
+
 	/**
 	 * Handles the {@link lombok.extern.slf4j.XSlf4j} annotation for Eclipse.
 	 */
